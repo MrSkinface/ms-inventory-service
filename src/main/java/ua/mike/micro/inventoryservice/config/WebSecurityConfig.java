a8bc2d1ebfc64ff3402b1deaaea96f9977a81190
Mike <levitskym@edin.ua>
2022-06-20 10:03:32 +0300

    ...

---
+++ src/main/java/ua/mike/micro/inventoryservice/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package ua.mike.micro.inventoryservice.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by mike on 20.06.2022 09:54
+ */
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
